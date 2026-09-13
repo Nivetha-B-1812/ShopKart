6bbd248624eeabd6fa0045768b4803797cde50f2
Nivetha Baskar <nivi182006@gmail.com>
2026-09-13 13:23:51 +0530

    Fix Render database path

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    SECRET_KEY = "shopkart-development-secret-key"
-
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "database", "shopkart.db")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "shopkart-development-secret-key"
     )
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/shopkart.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
